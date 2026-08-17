0a474ad4d29dde5ddb171384fda344954b24e9fd
1234 <a1234@kuma.local>
2026-08-17 16:19:24 +0800

    chore: update base path to '/' for custom domain dbystudy.cn

--- vite.config.ts
+++ vite.config.ts
@@ -51,7 +51,7 @@ function contentStatusPlugin(): Plugin {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/beijing-study-portal/',
+  base: '/',
   plugins: [react(), contentStatusPlugin()],
   build: {
     rollupOptions: {
